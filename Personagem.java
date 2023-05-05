8decc0cb9f78120e1f56b2cc9bb00ee6e0940e57
Akaxys <132687789+Akaxys@users.noreply.github.com>
2023-05-05 12:48:38 -0300

    Personagem-Java

---
+++ Personagem.java
@@ -0,0 +1,98 @@
+import java.util.*;
+
+public class Personagem {
+    private String name;
+    private int vida;
+    private int mana;
+    private int vigor;
+    private int bonusAtaque = 0;
+    private int bonusDefesa = 0;
+    private int pocaoCura = 3;
+    private int pocaoMana = 3;
+
+
+    Inimigo soldado = new Inimigo("Soldade", 30);
+
+
+
+    //constructor
+    public Personagem(String n){
+      this.name = n;
+    }
+    public Personagem(int pv){
+        this.vida = pv;
+    }
+
+    
+        // Nome
+    public String getNome() {
+        return name;
+    }
+    public void setNome(String nome) {
+        this.name = nome; 
+    }
+
+        // Vida
+    public int getVida() {
+        return vida;
+    }
+    public void setVida(int pv) {
+        this.vida = pv; 
+    }
+
+        // Mana
+    public int getMana() {
+        return mana;
+    }
+    public void setMana(int pm) {
+        this.mana = pm;
+    }
+
+        // Vigor 
+    public int getVigor() {
+        return vigor;
+    }
+    public void setVigor(int vig) {
+        this.vigor = vig;
+    }
+
+        // Bonus Ataque
+    public int getbonusAtaque() {
+        return bonusAtaque;
+    }
+    public void setbonusAtaque(int bonusAtaque) {
+        this.bonusAtaque += bonusAtaque; 
+    }
+
+        // Bonus Defesa
+    public int getbonusDefesa() {
+        return bonusDefesa;
+    }
+    public void setbonusDefesa(int bonusDefesa) {
+        this.bonusDefesa += bonusDefesa; 
+    }
+
+        // Poção de Cura
+    public int getPocaoCura() {
+        return pocaoCura;
+    }
+    public void setPocaoCura(int gastPocao){
+        this.pocaoCura = gastPocao;
+    }
+
+        // Poção de Mana
+    public int getPocaoMana() {
+        return pocaoMana;
+    }
+    public void setPocaoMana(int gastPocao){
+        this.pocaoMana = gastPocao;
+    }
+
+
+    
+    
+
+
+
+}
+  
